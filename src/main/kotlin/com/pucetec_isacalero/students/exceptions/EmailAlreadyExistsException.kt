08fa32634bed6bb018c3a87255f787e6cfc71cec
IsaacCalero <iacalero@puce.edu.ec>
2026-06-01 20:48:47 -0500

    Commit-2

---
+++ src/main/kotlin/com/pucetec_isacalero/students/exceptions/EmailAlreadyExistsException.kt
@@ -0,0 +1,4 @@
+package com.pucetec_isacalero.students.exceptions
+
+class EmailAlreadyExistsException {
+}
---
+++ src/main/kotlin/com/pucetec_isacalero/students/mappers/StudentMapper.kt
@@ -0,0 +1,4 @@
+package com.pucetec_isacalero.students.mappers
+
+class StudentMapper {
+}
